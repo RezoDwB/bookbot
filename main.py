905595d3cdd31bda1bab9899124ef98fd169c1a6
RezoDwB <brent.whitehead24@gmail.com>
2025-08-27 21:26:09 +1000

    Created reporting function to stats.py and main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_chars_dict
+from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 
 def get_book_text(fPath):
     with open(fPath) as f:
@@ -10,7 +10,18 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(num_words,"words found in the document")
-    print(chars_dict)
+    sorted_list = chars_dict_to_sorted_list(chars_dict)
+    #print(num_words,"words found in the document")
+    #print(chars_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for items in sorted_list:
+        if items["char"].isalpha():
+            print(f"{items['char']}: {items['num']}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,14 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+def sort_on(items):
+    return items["num"]
+
+def chars_dict_to_sorted_list(char_dict):
+    sorted_list = []
+    for char, num in char_dict.items():
+        char_dict_entry = {"char": char, "num": num}
+        sorted_list.append(char_dict_entry)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
